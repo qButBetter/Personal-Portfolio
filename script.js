20e31097fb9f712afd2eefeaa98f31138bb60cbd
qButBetter <james.steed23@student.cart.org>
2024-04-21 21:58:33 -0700

    got some of the sounds to work on click :fire:

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 var quagsireAud = new Audio("Sounds/quagsire.mp3");
 var brickAud = new Audio("Sounds/brick-house.mp3")
 
-document.getElementById("quagsire").addEventListener("click", function() {
 
-    quagsireAud.play();
-    console.log("Quagsire Clicked!")
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("quagsire").addEventListener("click", function() {
 
+        quagsireAud.play();
+        console.log("Quagsire Clicked!")
+    
+    });
+
+    document.getElementById("brick-wall").addEventListener("click", function () {
+        brickAud.play();
+        console.log("Wall Clicked!");
+    });
 });
