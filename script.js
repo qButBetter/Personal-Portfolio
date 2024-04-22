b915b12ba624918be3fa2ff1c710a2e8d1eaa297
qButBetter <143832384+qButBetter@users.noreply.github.com>
2024-04-22 08:38:41 -0700

    got brick house to work, along with a counter

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ var quagsireAud = new Audio("Sounds/quagsire.mp3");
 var brickAud = new Audio("Sounds/brick-house.mp3")
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("quagsire").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
+    let quagCount = 0;
+
+    document.getElementById("quagsire").addEventListener("click", function () {
 
         quagsireAud.play();
-        console.log("Quagsire Clicked!")
-    
-    });
+        console.log("Quagsire Clicked!");
+        quagCount++;
+        console.log("Quagsire Counter: " + quagCount);
 
+        if (quagCount > 99) {
+            // Do something silly with this! ;-;
+            window.open("https://www.tacobell.com/");
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("brick-wall").addEventListener("click", function () {
         brickAud.play();
         console.log("Wall Clicked!");
